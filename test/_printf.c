2b6e6fdc13df625253c16caeba554a19fe329a58
Madiocre <wolvirex@gmail.com>
2023-09-16 17:06:22 -0700

    test_directory_added

---
+++ test/_printf.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * _printf_char - helper func to print char
+ * @c: char to print
+ * Return: Number of characters
+ */
+int _printf_char(int c)
+{
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * _printf_string - helper func to print strings
+ * @str: string to print
+ * Return: Number of characters
+ */
+int _printf_string(const char *str)
+{
+	int i, len;
+
+	if (str == NULL)
+		return (1);
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (len);
+}
+
+/**
+ * _printf_ukn - print unknown characters
+ * @c: character to print
+ * Return: Number of characters
+ */
+int _printf_ukn(char c)
+{
+	_putchar(c);
+	return (2);
+}
+
+
+/**
+ * _printf - Custom printf function implementation.
+ * @format: Format string containing conversion specifiers *
+ * Return: Number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list vars;
+
+	va_start(vars, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += _printf_char(va_arg(vars, int));
+					break;
+				case 's':
+					count += _printf_string(va_arg(vars, const char *));
+					break;
+				case '%':
+					count += _printf_char('%');
+					break;
+				default:
+					count += _printf_ukn(*format);
+					break;
+			}
+		}
+		else
+		{
+			count += _printf_char(*format);
+		}
+		format++;
+	}
+	va_end(vars);
+	return (count);
+}
---
+++ test/main.h
@@ -0,0 +1,12 @@
+#ifndef _PRINTF_H_
+#define _PRINTF_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
