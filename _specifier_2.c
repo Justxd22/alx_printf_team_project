74b270bdf034e82906004a838d145e9749547343
justxd22 <xdjust18@gmail.com>
2023-09-19 04:23:57 +0300

    fix_special

--- _specifier_2.c
+++ _specifier_2.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _printf_string_special - handles %S specifier
  * @buffer: buffer to check
@@ -9,8 +10,7 @@
  */
 int _printf_string_special(char *buffer, char *buffer_ptr, const char *S)
 {
-	int i = 0, j, len = 0, buff = 0, dec;
-	char *literal = "\\x", str[2];
+	int i = 0, len = 0, temp;
 
 	if (S == NULL)
 		return (0);
@@ -18,35 +18,23 @@ int _printf_string_special(char *buffer, char *buffer_ptr, const char *S)
 	flush_buffer(buffer, buffer_ptr);
 	while (S[i] != '\0')
 	{
-		if ((S[i] > 0 && S[i] < 32) || S[i] >= 127)
+		if (S[i] < 32 || S[i] >= 127)
 		{
-			for (j = 0; literal[j] != '\0'; j++)
-			{
-				flush_buffer(buffer, buffer_ptr);
-				*buffer_ptr = literal[j], buffer_ptr++;
-			}
-			len += 2, dec = S[i];
-			for (j = 0; j < 2; j++)
-			{
-				buff = dec % 16;
-				if (buff < 10)
-					buff += '0';
-				else
-					buff += ('0' + 7);
-				str[j] = buff, dec /= 16;
-			}
-			while (j >= 0)
-			{
-				flush_buffer(buffer, buffer_ptr);
-				*buffer_ptr = str[j], buffer_ptr++, j--;
-			}
-			len += 2, i++;
+			flush_buffer(buffer, buffer_ptr);
+			*buffer_ptr = '\\';
+			buffer_ptr++;
+			*buffer_ptr = 'x';
+			buffer_ptr++;
+			len += 2;
+			temp = _printf_hexa_cap(buffer, buffer_ptr, (unsigned int)S[i]);
+			len += temp, buffer_ptr += temp;
 		}
 		else
 		{
 			flush_buffer(buffer, buffer_ptr);
-			*buffer_ptr = S[i], buffer_ptr++, len++, i++;
+			*buffer_ptr = S[i], buffer_ptr++, len++;
 		}
+		i++;
 	}
 	return (len);
 }
